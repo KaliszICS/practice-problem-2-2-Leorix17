e84748e800a5b76524215ed6086d2fe52b707b69
Leorix17 <343430617@tdsb.ca>
2026-03-30 15:19:27 +0000

    fin

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,39 @@
+/**
+ * File: More Functions
+ * Author: Leo
+ * Date Created: Mar 30, 2026
+ * Date Last Modified: Mar 30, 2026
+ */
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
 	}
 
+	public static boolean isEven(int num) {
+		boolean num1 = (num % 2) == 0;
+		return num1;
+	}
+
+	public static boolean isOdd(int num) {
+		boolean num1 = (num % 2) != 0;
+		return num1;
+	}
+
+	public static boolean isPositive(int num) {
+		boolean num1 = (num > 0);
+		return num1;
+	}
+
+	public static boolean isNegative(int num) {
+		boolean num1 = (num < 0);
+		return num1;
+	}
+
+	public static int combinedLength(String word1, String word2) {
+		String word = word1 + word2;
+		int length1 = word.length();
+		return length1;
+
+	}
 }
